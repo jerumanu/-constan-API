fe2861a151db0415d76b9050de0d334e3ef4a3ac
jerumanu <emmanueljeru200@gmail.com>
2021-08-04 13:38:24 +0300

    add urls to authentication

---
+++ authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path("", views.LoginAPIView.as_view(), name=""),
+    # path("register", views.RegistrationAPIView.as_view(), name="register"),
+]
